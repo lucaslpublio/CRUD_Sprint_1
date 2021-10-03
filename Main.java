b23304ca533519818d5d0cd3650d80327b71e23e
OGatoFelix <78486705+OGatoFelix@users.noreply.github.com>
2021-10-03 15:53:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,24 +14,24 @@ public class Main {
       Usuario c3 = new Usuario("Pedro Augusto Couves", "pcouves@gmail.com", 01245, "R3");
 
       // ESCRITA
-      arqIndexado = new ArquivoIndexado<>("usuarios", Usuario.class.getConstructor());
+      arqUsuarios = new ArquivoIndexado<>("usuarios", Usuario.class.getConstructor());
 
-      arqIndexado.create(c1);
-      arqIndexado.create(c2);
-      arqIndexado.create(c3);
+      arqUsuarios.create(c1);
+      arqUsuarios.create(c2);
+      arqUsuarios.create(c3);
 
-      Usuario c0 = arqIndexado.read(1);
+      Usuario c0 = arqUsuarios.read(1);
       System.out.println(c0);
 
-      Usuario c01 = arqIndexado.read(2);
+      Usuario c01 = arqUsuarios.read(2);
       System.out.println(c01);
 
-      arqIndexado.delete(2);
-      System.out.println("\n" + arqIndexado.read(2));
+      arqUsuarios.delete(2);
+      System.out.println("\n" + arqUsuarios.read(2));
 
       c0.nome = "José das Couves";
-      arqIndexado.update(c0);
-      System.out.println("\n" + arqIndexado.read(1));
+      arqUsuarios.update(c0);
+      System.out.println("\n" + arqUsuarios.read(1));
 
     } catch (Exception e) {
       e.printStackTrace();
